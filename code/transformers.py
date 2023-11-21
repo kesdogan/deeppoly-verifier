8db86deee48d8b47a485410ddc39864bb61ba606
Juraj Mičko <micko.juraj64@gmail.com>
2023-11-21 12:36:20 +0100

    first draft of Polygon and Linear layer

--- code/transformers.py
+++ code/transformers.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 import torch
 from torch import Tensor
 
+import logging
+
 
 @dataclass
 class Polygon:
@@ -15,58 +17,62 @@ class Polygon:
     We hereby refer to the shape of weights as the shape of the Polygon itself.
     """
 
-    l_coefs: Tensor  # (batch, *out, *source)
-    l_bias: Tensor  # (batch, *out)
+    l_coefs: Tensor  # (batch, out, source)
+    l_bias: Tensor  # (batch, out)
 
-    u_coefs: Tensor  # (batch, *out, *source)
-    u_bias: Tensor  # (batch, *out)
+    u_coefs: Tensor  # (batch, out, source)
+    u_bias: Tensor  # (batch, out)
 
     @staticmethod
-    def create_from_input(x: Tensor) -> "Polygon":
+    def create_from_input(input_shape: torch.Size) -> "Polygon":
         """
-        - input shape: (batch, *source)
-        - output: Polygon with shape (batch, *source, *source),
-        i.e. behaves like an identity layer where *out == *source.
+        - input shape: (batch, source)
+        - output: Polygon with shape (batch, source, source),
+        i.e. behaves like an identity layer where out == source.
         """
-        batch, *input_shape = x.shape
-        filler_shape = [1 for _ in input_shape]
-        N = torch.prod(torch.tensor(input_shape)).item()
-        p_tensor = torch.arange(N).reshape(*input_shape, *filler_shape)
-        i_tensor = torch.arange(N).reshape(*filler_shape, *input_shape)
+        batch, *dims = input_shape
+        input_size = torch.prod(torch.tensor(dims)).item()
 
-        eye = (p_tensor == i_tensor).float()
-
-        return Polygon(
-            l_coefs=eye.repeat(batch, 1),
-            l_bias=torch.zeros_like(x),
-            u_coefs=eye.repeat(batch, 1),
-            u_bias=torch.zeros_like(x),
+        polygon = Polygon(
+            l_coefs=torch.eye(input_size).repeat(batch, 1),
+            l_bias=torch.zeros((batch, input_size)),
+            u_coefs=torch.eye(input_size).repeat(batch, 1),
+            u_bias=torch.zeros((batch, input_size)),
         )
+        logging.debug(f"Created Polygon {polygon}")
 
-    def _mul_last_k(self, x: Tensor, y: Tensor, k: int) -> Tensor:
-        """
-        Perform a scalar product on the last k dimensions of x and y.
-        """
-        assert x.shape == y.shape
-        shape = x.shape[:-k]
-        return torch.einsum("...i,...i->...", x.view(*shape, -1), y.view(*shape, -1))
+        return polygon
+
+        # def _mul_last_k(self, x: Tensor, y: Tensor, k: int) -> Tensor:
+        #     """
+        #     Perform a scalar product on the last k dimensions of x and y.
+        #     """
+        #     assert x.shape == y.shape
+        #     shape = x.shape[:-k]
+        #     return torch.einsum("...i,...i->...", x.view(*shape, -1), y.view(*shape, -1))
 
     def _get_bound(self, x: Tensor, eps: float, lower: bool) -> Tensor:
+        x = x.reshape(x.shape[0], -1)
+
         if lower:
             weight, bias = self.l_coefs, self.l_bias
+            # (batch, out, source)
             condition = weight > 0
         else:
             weight, bias = self.u_coefs, self.u_bias
+            # (batch, out, source)
             condition = weight < 0
 
+        # (batch, 1, source)
         x_lb, x_ub = (x - eps).unsqueeze(1), (x + eps).unsqueeze(1)
-        input_dim = len(x.shape) - 1
+        # input_dim = len(x.shape) - 1
 
-        # (batch, *out, *source)
+        # (batch, out, source)
         combination = torch.where(condition, x_lb, x_ub)
 
-        # (batch, *out)
-        return self._mul_last_k(weight, combination, k=input_dim) + bias
+        # (batch, out)
+        # return torch.einsum("...i,...i->...", weight, combination) + bias
+        return (weight * combination).sum(-1) + bias
 
     def evaluate(self, x: Tensor, eps: float) -> tuple[Tensor, Tensor]:
         """
@@ -84,60 +90,12 @@ class FlattenTransformer(torch.nn.Module):
         super().__init__()
 
     def forward(self, x: Polygon) -> Polygon:
-        batch_size = x.l_coefs.shape[0]
-        return Polygon(
-            l_coefs=x.l_coefs.reshape(batch_size, -1),
-            l_bias=x.l_bias,
-            u_coefs=x.u_coefs,
-            u_bias=x.u_bias,
-        )
+        return x
 
 
 class LinearTransformer(torch.nn.Module):
-    # neuron = SUM_i ( weight_i * input_i ) + bias
-
-    # parameters from the network:
-    # - weight[m, n]
-    # - bias[n]
-
-    # i - iterates over input space
-    # n - iterates over the current layer
-    # m - iterates over the previous layer
-
-    # l_constraint[m] = SUM_i ( l_coefs[m, i] * input_i ) + l_bias[m]
-    # u_constraint[m] = SUM_i ( u_coefs[m, i] * input_i ) + u_bias[m]
-
-    # neuron >= SUM_n ( weight_i * (l_constraints or u_constraints) ) + bias
-    # neuron <= SUM_n ( weight_i * (u_constraints or l_constraints) ) + bias
-
-    # l_constraint[n] = SUM_m (
-    #               weight[m, n]
-    #               * ( [weight[m, n] > 0] * l_constraint[m] + [weight[m, n] <= 0] * u_constraint[m] )
-    #           ) + bias[n]
-    # u_constraint[n] = SUM_m (
-    #               weight[m, n]
-    #               * ( [weight[m, n] > 0] * u_constraint[m] + [weight[m, n] <= 0] * l_constraint[m] )
-    #           ) + bias[n]
-
-    # l_coefs[n, i] = SUM_m (
-    #     weight[m, n]
-    #     * ( [weight[m, n] > 0] * l_coefs[m, i] + [weight[m, n] <= 0] * u_coefs[m, i] )
-    #     )
-
-    # l_bias[n] = SUM_m (
-    #     weight[m, n]
-    #     * ( [weight[m, n] > 0] * l_bias[m] + [weight[m, n] <= 0] * u_bias[m] )
-    #     ) + bias[n]
-
-    # l_coefs_2[n, i] = SUM_m (
-    #     weight[m, n]
-    #     * l_coefs[m, i]
-    #     )
-
-    # for a neuron: l_coefs[i] = weight * SUM_m ( [weight[m, i] > 0] * l_coefs[m, i] + [weight[m, i] <= 0] * u_coefs[m, i] )
-
-    weight: Tensor  # (in_size, out_size)
-    bias: Tensor  # (out_size)
+    weight: Tensor  # (out, in)
+    bias: Tensor  # (out)
 
     def __init__(self, weight: Tensor, bias: Tensor):
         super().__init__()
@@ -146,32 +104,26 @@ class LinearTransformer(torch.nn.Module):
 
     def forward(self, x: Polygon) -> Polygon:
         """
-        - x shape: (batch, in_size, *source)
-        - output shape: (batch, out_size, *source)
+        - x shape: (batch, in, input_size)
+        - output shape: (batch, out,  input_size)
         """
-
-        # TODO Shapes don't make sense
-        # (out_size, in_size, *source)
-        l_coefs = torch.where(
-            self.weight.unsqueeze(-1) > 0,  # (out_size, in_size, 1)
-            x.l_coefs,  # (in_size, *source)
-            x.u_coefs,  # (in_size, *source)
+        l_coefs = (
+            torch.clamp(self.weight, min=0) @ x.l_coefs
+            + torch.clamp(self.weight, max=0) @ x.u_coefs
         )
-        # (out_size, *source)
-        l_coefs_1 = self.weight.T @ l_coefs
-
-        l_coefs_2 = (
-            torch.clamp(self.weight.T, 0, None) @ x.l_coefs
-            + torch.clamp(self.weight.T, None, 0) @ x.u_coefs
+        u_coefs = (
+            torch.clamp(self.weight, min=0) @ x.u_coefs
+            + torch.clamp(self.weight, max=0) @ x.l_coefs
         )
-        assert l_coefs_1 == l_coefs_2
-        l_coefs = l_coefs_1
+        l_bias = (
+            (torch.clamp(self.weight, min=0) * x.l_bias).sum(-1)
+            + (torch.clamp(self.weight, max=0) * x.u_bias).sum(-1)
+        ) + self.bias
+        u_bias = (
+            (torch.clamp(self.weight, min=0) * x.u_bias).sum(-1)
+            + (torch.clamp(self.weight, max=0) * x.l_bias).sum(-1)
+        ) + self.bias
 
-        u_coefs = self.weight.T @ torch.where(
-            self.weight.T.unsqueeze(-1) > 0, x.u_coefs, x.l_coefs
-        )
-
-        l_bias = self.weight.T @ x.l_coefs + self.bias
-        u_bias = self.weight.T @ x.u_coefs + self.bias
-
-        l_bound = l_coefs @ self.inputs
+        polygon = Polygon(l_coefs=l_coefs, l_bias=l_bias, u_coefs=u_coefs, u_bias=u_bias, )
+        logging.debug(f"Linear layer outputs Polygon {polygon}")
+        return polygon
--- code/verifier.py
+++ code/verifier.py
@@ -1,16 +1,47 @@
 import argparse
-import torch
+import logging
 
+import torch
+import torch.nn as nn
+
+from transformers import LinearTransformer, FlattenTransformer, Polygon
 from networks import get_network
 from utils.loading import parse_spec
 
 DEVICE = "cpu"
 
+torch.set_printoptions(threshold=10_000)
+logging.basicConfig(level=logging.DEBUG)
+
 
 def analyze(
-    net: torch.nn.Module, inputs: torch.Tensor, eps: float, true_label: int
+    net: torch.nn.Sequential, inputs: torch.Tensor, eps: float, true_label: int
 ) -> bool:
-    return 0
+    # add the 'batch' dimension
+    inputs = inputs.unsqueeze(0)
+
+    # Construct a model like net that passes Polygon through each layer
+    layers = []
+    for layer in net.children():
+        if isinstance(layer, torch.nn.Flatten):
+            layers.append(FlattenTransformer())
+        if isinstance(layer, torch.nn.Linear):
+            layers.append(LinearTransformer(layer.weight, layer.bias))
+    polygon_model = nn.Sequential(*layers)
+
+    in_polygon = Polygon.create_from_input(inputs.shape)
+    out_polygon = polygon_model(in_polygon)
+    lower_bounds, upper_bounds = out_polygon.evaluate(inputs, eps)
+
+    # noinspection PyTypeChecker
+    verified = torch.all(
+        torch.cat(
+            (upper_bounds[:, :true_label], upper_bounds[:, (true_label + 1) :]), dim=1
+        )
+        < lower_bounds[:, true_label]
+    ).item()
+
+    return verified
 
 
 def main():
@@ -39,6 +70,11 @@ def main():
         help="Neural network architecture which is supposed to be verified.",
     )
     parser.add_argument("--spec", type=str, required=True, help="Test case to verify.")
+    parser.add_argument(
+        "--check",
+        help="Whether to check the GT answer.",
+        action=argparse.BooleanOptionalAction,
+    )
     args = parser.parse_args()
 
     true_label, dataset, image, eps = parse_spec(args.spec)
@@ -53,10 +89,19 @@ def main():
     pred_label = out.max(dim=1)[1].item()
     assert pred_label == true_label
 
-    if analyze(net, image, eps, true_label):
-        print("verified")
-    else:
-        print("not verified")
+    verified = analyze(net, image, eps, true_label)
+    verified_text = "verified" if verified else "not verified"
+    print(verified_text)
+
+    if args.check:
+        with open("test_cases/gt.txt", "r") as f:
+            for line in f.read().splitlines():
+                model, fl, answer = line.split(",")
+                if model == args.net and fl in args.spec:
+                    if verified_text == answer:
+                        print("^ correct\n")
+                    else:
+                        print(f"incorrect, expected {answer}\n")
 
 
 if __name__ == "__main__":
